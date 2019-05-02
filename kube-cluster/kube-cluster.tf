65223703cc9d6e5a5d21bc75bb5b1a146be46d86
sstanytska <stanytskasofia@gmail.com>
2019-05-01 20:01:39 -0500

    cluster

--- kube-cluster/kube-cluster.tf
+++ kube-cluster/kube-cluster.tf
@@ -1,35 +1,19 @@
-resource "google_container_cluster" "gke-cluster" {
-    name                = "gke-cluster"
-    network             = "default"
-    # subnetwork          = "us-central1"
-    location            = "us-central1"
-    min_master_version  = "1.11.8-gke.6"
-    # initial_node_count  = 1
-    remove_default_node_pool = true
+resource "google_container_cluster" "gcpcluster" {
+  project    = "astral-root-234422"
+  name       = "gcpcluster"
+  network    = "default"
+  subnetwork = "default"
+  zone       = "us-central1-a"
 
-    node_pool {
-      name = "default-pool"
+  # additional_zones = "${var.additional_zones}"
+
+  master_auth {
+    username = "admin"
+    password = "StrongP1~assworafasfweaqg32d"
   }
-}
-
-resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name       = "snoop-dogg-pool"
-  location   = "us-central1"
-  cluster    = "${google_container_cluster.gke-cluster.name}"
-  node_count = 1
-
+  initial_node_count = "2"
   node_config {
-    preemptible  = true
-    machine_type = "n1-standard-1"
-
-    metadata {
-      disable-legacy-endpoints = "true"
-    }
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+    # machine_type = ""
+    # oauth_scopes = [  #   "https://www.googleapis.com/auth/compute",  #   "https://www.googleapis.com/auth/devstorage.read_only",  #   "https://www.googleapis.com/auth/logging.write",  #   "https://www.googleapis.com/auth/monitoring",  # ]
   }
 }
