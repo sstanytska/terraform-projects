c72b8432646b5d8c5167e012fef7868474909db3
sstanytska <stanytskasofia@gmail.com>
2019-05-04 18:47:20 -0500

    change account

--- kube-cluster/kube-cluster.tf
+++ kube-cluster/kube-cluster.tf
@@ -3,7 +3,7 @@ provider "google" {
   project       = "${var.project}"
   region        = "${var.region}"
 }
-resource "google_container_cluster" "gosonik" {
+resource "google_container_cluster" "gosonikmurod" {
     name                = "${var.cluster_name}"
     network             = "default"
     subnetwork          = "default"
@@ -18,7 +18,7 @@ resource "google_container_cluster" "gosonik" {
   #  zone       = "us-central1-a"
   # additional_zones = "${var.additional_zones}"
   node_config {
-     machine_type = "n1-standard-1"
+     machine_type = "n1-standard-2"
     # oauth_scopes = [  #   "https://www.googleapis.com/auth/compute",  #   "https://www.googleapis.com/auth/devstorage.read_only",  #   "https://www.googleapis.com/auth/logging.write",  #   "https://www.googleapis.com/auth/monitoring",  # ]
   }
 }
--- service-account gcp/variables.tf
+++ service-account gcp/variables.tf
@@ -1,14 +1,14 @@
 variable "account_id" {
     description = "The service account ID. Changing this forces a new service account to be created."
-    default     = "astral-root-234422"
+    default     = "civil-celerity-239622"
 }
 variable "display_name" {
     description = "The display name for the service account. Can be updated without creating a new resource."
-    default     = "sonickS"
+    default     = "sonickmurod"
 }
 variable "roles" {
     type        = "list"
     description = "The roles that will be granted to the account."
-    default     = ["roles/cloudsql.admin"]
+    default     = ["roles/editor",]
 }
 variable "project" {}
