8211f08ce35785374de7957bed6827224c5a4112
sstanytska <stanytskasofia@gmail.com>
2019-05-04 22:41:40 -0500

    output change

--- service-account gcp/outputs.tf
+++ service-account gcp/outputs.tf
@@ -1,12 +1,12 @@
 output "email" {
-    value       = "${google_service_account.sonick.email}"
+    value       = "${google_service_account.zver.email}"
     description = "The e-mail address of the service account."
 }
 output "name" {
-    value       = "${google_service_account.sonick.name}"
+    value       = "${google_service_account.zver.name}"
     description = "The fully-qualified name of the service account."
 }
 output "unique_id" {
-    value       = "${google_service_account.sonick.unique_id}"
+    value       = "${google_service_account.zver.unique_id}"
     description = "The unique id of the service account."
 }
