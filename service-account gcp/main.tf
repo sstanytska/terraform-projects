9a7b376245ba70ce46453cbec4cc6424a640e22b
sstanytska <stanytskasofia@gmail.com>
2019-05-04 21:24:40 -0500

    added zver

--- service-account gcp/main.tf
+++ service-account gcp/main.tf
@@ -15,14 +15,14 @@ resource "google_service_account_key" "mykey" {
 #   valid_before        = "2014-10-02T15:01:23.045123456Z"  (The key can be used before this timestamp. A timestamp in RFC3339 UTC "Zulu" format, accurate to nanoseconds.) 
 }
 
-resource "google_project_iam_member" "sstanytska" {
-    depends_on        = ["google_service_account.sonick"]
+resource "google_project_iam_member" "zver_murod" {
+    depends_on        = ["google_service_account.zver"]
     count             =  "${length(var.roles)}"
     role              =  "${element(var.roles, count.index)}"
-    member            =  "serviceAccount:${google_service_account.sonick.email}"
+    member            =  "serviceAccount:${google_service_account.zver.email}"
 }
 
 resource "local_file" "privateKey" {
     content     = "${base64decode(google_service_account_key.mykey.private_key)}"
-    filename    = "/tmp/private_key.json"
+    filename    = "tmp/private_key.json"
 }
